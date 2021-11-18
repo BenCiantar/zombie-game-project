2e94ef508c9a0e402c92821788be14dbce811760
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-11-18 11:54:54 +0100

    Added file structure and very basic pseudocode

---
+++ scripts/Game.js
@@ -0,0 +1,17 @@
+//DECLARE global variables
+
+function preload() {
+//LOAD assets
+}
+
+function create() {
+//GENERATE objects that are present on load
+}
+
+function update() {
+//GAME LOGIC GOES HERE
+}
+
+//FUNCTIONS THAT THE GAME USES GO DOWN HERE
+
+export default Game;
