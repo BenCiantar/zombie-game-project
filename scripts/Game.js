c25255050b2fca5afb591c10fda61fa0152faa74
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-11-18 16:45:22 +0100

    Fixed image issue

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,17 +1,27 @@
 //DECLARE global variables
+let center, player;
 
-function preload() {
-//LOAD assets
+import playerImageSrc from "../assets/player_9mm.png";
+
+class Game extends Phaser.Scene {
+    preload() {
+        this.load.image("player", playerImageSrc);
+    }
+
+    create() {
+        center = {
+            x:this.physics.world.bounds.width / 2,
+            y:this.physics.world.bounds.height / 2
+        }
+
+        player = this.physics.add.sprite(center.x, center.y, "player");
+
+    }
+
+    update() {
+
+    }
 }
-
-function create() {
-//GENERATE objects that are present on load
-}
-
-function update() {
-//GAME LOGIC GOES HERE
-}
-
 //FUNCTIONS THAT THE GAME USES GO DOWN HERE
 
 export default Game;
