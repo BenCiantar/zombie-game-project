3e6d3e8365362cf6337a2736b4c83e8e7e3d0599
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-11-21 09:15:15 +0100

    Added player controls and mapped them to WASD keys

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,5 +1,5 @@
 //DECLARE global variables
-let center, player;
+let center, player, gameStarted, playerControls;
 
 import playerImageSrc from "../assets/player_9mm.png";
 
@@ -16,10 +16,39 @@ class Game extends Phaser.Scene {
 
         player = this.physics.add.sprite(center.x, center.y, "player");
 
+        gameStarted = true; //Set this to the startgame button on the menu
+        
+        //Initialise playerControls with directions mapped to arrow keys
+        playerControls = this.input.keyboard.createCursorKeys();
+
+        //Remap WASD keys to up, down, left and right
+        playerControls = this.input.keyboard.addKeys(
+            {up:Phaser.Input.Keyboard.KeyCodes.W,
+            down:Phaser.Input.Keyboard.KeyCodes.S,
+            left:Phaser.Input.Keyboard.KeyCodes.A,
+            right:Phaser.Input.Keyboard.KeyCodes.D});
     }
 
     update() {
+        if (gameStarted) {
+            if (playerControls.left.isDown) {
+                player.setVelocityX(-160);
+            }
+            else if (playerControls.right.isDown) {
+                player.setVelocityX(160);
+            } else {
+                player.setVelocityX(0);
+            } 
 
+            if (playerControls.up.isDown) {
+                player.setVelocityY(-160);
+            } 
+            else if (playerControls.down.isDown) {
+                player.setVelocityY(160);
+            } else {
+                player.setVelocityY(0);
+            }
+        }
     }
 }
 //FUNCTIONS THAT THE GAME USES GO DOWN HERE
