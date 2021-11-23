7b591724dda0fb0c7bd7b0cd49e6d1fecc8c6891
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-11-23 13:15:50 +0100

    Zombies turning to face and follow player

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,5 +1,5 @@
 //Declare global variables
-let positions, player, gameStarted, playerControls, basicZombie;
+let positions, player, gameStarted, playerControls, basicZombie, zombies, thisGame;
 
 import playerImageSrc from "../assets/player_9mm.png";
 import bgImageSrc from "../assets/bg-mud.png";
@@ -15,6 +15,7 @@ class Game extends Phaser.Scene {
     }
 
     create() {
+        thisGame = this;
         positions = {
             centerX: this.physics.world.bounds.width / 2,
             centerY: this.physics.world.bounds.height / 2,
@@ -60,62 +61,48 @@ class Game extends Phaser.Scene {
             repeat: -1
         });
         // spawnZombie(this, basicZombie, "zombiebasic");
-        basicZombie = this.physics.add.sprite(positions.centerX - 100, positions.centerY - 100, "zombiebasic");
-        basicZombie.setScale(0.65);
-        basicZombie.anims.play("zombiebasic");
+        zombies = this.physics.add.group();
+
+        // basicZombie = this.physics.add.sprite(positions.centerX - 100, positions.centerY - 100, "zombiebasic");
+        // basicZombie.setScale(0.65);
+        // basicZombie.anims.play("zombiebasic");
+
     }
 
     update() {
         if (gameStarted) {
 
-            this.physics.moveToObject(basicZombie, player, 70);
-            this.physics.add.collider(player, basicZombie, bounce, null, this);
+            // this.physics.moveToObject(basicZombie, player, 70);
+            this.physics.add.collider(player, zombies.getChildren(), bounce, null, this);
 
             //Listen for player movement inputs
             if (playerControls.left.isDown) {
                 player.setVelocityX(-160);
-                turnZombie(player);
+                turnZombies(player);
             }
             else if (playerControls.right.isDown) {
                 player.setVelocityX(160);
-                turnZombie(player);
+                turnZombies(player);
             } else {
                 player.setVelocityX(0);
-                turnZombie(player);
+                turnZombies(player);
             } 
 
             if (playerControls.up.isDown) {
                 player.setVelocityY(-160);
-                turnZombie(player);
+                turnZombies(player);
             } 
             else if (playerControls.down.isDown) {
                 player.setVelocityY(160);
-                turnZombie(player);
+                turnZombies(player);
             } else {
                 player.setVelocityY(0);
-                turnZombie(player);
+                turnZombies(player);
             }
-        }
 
-        let randomZombieSpawn = (Math.floor(Math.random() * 1000));
-        if (randomZombieSpawn > 900) {
-            let randomDirection = Math.floor(Math.random() * 4);
-            if (randomDirection == 0) {
-                basicZombie = this.physics.add.sprite((Math.floor(Math.random() * positions.rightEdge)), positions.topEdge - 20, "zombiebasic");
-                basicZombie.setScale(0.65);
-                basicZombie.anims.play("zombiebasic");
-            } else if (randomDirection == 1) {
-                basicZombie = this.physics.add.sprite(positions.rightEdge + 20, (Math.floor(Math.random() * positions.bottomEdge)), "zombiebasic");
-                basicZombie.setScale(0.65);
-                basicZombie.anims.play("zombiebasic");
-            } else if (randomDirection == 2) {
-                basicZombie = this.physics.add.sprite((Math.floor(Math.random() * positions.rightEdge)), positions.bottomEdge + 20, "zombiebasic");
-                basicZombie.setScale(0.65);
-                basicZombie.anims.play("zombiebasic");
-            } else if (randomDirection == 3) {
-                basicZombie = this.physics.add.sprite(positions.leftEdge - 20, (Math.floor(Math.random() * positions.bottomEdge)), "zombiebasic");
-                basicZombie.setScale(0.65);
-                basicZombie.anims.play("zombiebasic");
+            let randomZombieSpawn = (Math.floor(Math.random() * 1000));
+            if (randomZombieSpawn > 900) {
+                addZombies();
             }
         }
     }
@@ -135,30 +122,50 @@ const turn = function (pointer) {
         player.setAngle(angle);
     }
 
-    const turnZombie = function (playerPos) {
+const turnZombies = function (playerPos) {
+
+    zombies.getChildren().forEach(function(item) {
         let angle = Phaser.Math.RAD_TO_DEG * Phaser.Math.Angle.Between(
-            basicZombie.x, 
-            basicZombie.y, 
-            playerPos.x, 
-            playerPos.y);
+            item.x, 
+            item.y, 
+            player.x, 
+            player.y);
+    
+        item.setAngle(angle + 90);
+    }, thisGame);
+}
 
-        basicZombie.setAngle(angle + 90);
+//Collision event between player and zombies
+function bounce(player, zombie) {
+    player.setVelocity(0.1);
+    zombie.setVelocity(0.1);
+}
+
+
+function addZombies() {
+    spawnZombie()
+
+    Phaser.Utils.Array.Each(
+        zombies.getChildren(), thisGame.physics.moveToObject, thisGame.physics, player, 70)
+//  Phaser.Utils.Array.Each(
+    //  zombies.getChildren.anims.play("zombiebasic")
+//    )
+}
+
+function spawnZombie() {
+
+    let randomDirection = Math.floor(Math.random() * 4);
+    if (randomDirection == 0) {
+        zombies.create((Math.floor(Math.random() * positions.rightEdge)), positions.topEdge - 20, "zombiebasic").setScale(0.65);
+
+    } else if (randomDirection == 1) {
+        zombies.create(positions.rightEdge + 20, (Math.floor(Math.random() * positions.bottomEdge)), "zombiebasic").setScale(0.65);
+
+    } else if (randomDirection == 2) {
+        zombies.create((Math.floor(Math.random() * positions.rightEdge)), positions.bottomEdge + 20, "zombiebasic").setScale(0.65);
+
+    } else if (randomDirection == 3) {
+        zombies.create(positions.leftEdge - 20, (Math.floor(Math.random() * positions.bottomEdge)), "zombiebasic").setScale(0.65);
+
     }
-
-    function bounce(player, basicZombie) {
-        player.setVelocity(0.1);
-        basicZombie.setVelocity(0.1);
-
-    }
-
-
-    // const spawnZombie = function (game, type, ref) {
-
-    // }
-
-    function spawnZombieBottom() {
-
-    }
-
-
-
+}
