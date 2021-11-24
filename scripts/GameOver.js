fd08ad19915bb7770eb49f64b2fc8254f3060dd6
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-11-24 13:35:48 +0100

    Added basic scene info for game over screen

--- scripts/GameOver.js
+++ scripts/GameOver.js
@@ -1,19 +1,33 @@
 //Declare global variables
-let thisGameOver;
+let positions, thisGameOver, gameOverText;
 
 class GameOver extends Phaser.Scene {
     constructor() { 
         super({key: 'GameOverScene'}); 
     }
-    
-    preload() {
+
+     preload() {
 
     }
 
-    
+
     create() {
+        positions = {
+            centerX: this.physics.world.bounds.width / 2,
+            centerY: this.physics.world.bounds.height / 2,
+            topEdge: 0,
+            rightEdge: this.physics.world.bounds.width,
+            bottomEdge: this.physics.world.bounds.height,
+            leftEdge: 0
+        };
+
         thisGameOver = this;
 
+        gameOverText = this.add.text(positions.centerX, positions.centerY, 'Game Over!', { 
+            fill: '#FFFFFF', 
+            fontSize: 50,
+            color: "#FFFFFF",
+        }).setOrigin(0.5);
     }
 
 
