ec889c6fa90b2d4a685479e20df90d5aa9e10039
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-11-24 13:27:52 +0100

    Added a basic button and added event listeners with descriptive text

--- scripts/Menu.js
+++ scripts/Menu.js
@@ -1,29 +1,41 @@
 //Declare global variables
-let thisMenu, text;
+let positions, thisMenu;
 
 class Menu extends Phaser.Scene {
     constructor() { 
         super({key: 'MenuScene'}); 
     }
-    
+
     preload() {
 
     }
 
 
     create() {
+        positions = {
+            centerX: this.physics.world.bounds.width / 2,
+            centerY: this.physics.world.bounds.height / 2,
+            topEdge: 0,
+            rightEdge: this.physics.world.bounds.width,
+            bottomEdge: this.physics.world.bounds.height,
+            leftEdge: 0
+        };
+
         thisMenu = this;
 
-        text = this.add.text(
-            640, 
-            360, 
-            "Hello World", 
-            {
-                fontSize: 50,
-                color: "#FFFFFF",
-                fontStyle: "bold"
-            }
-        ).setOrigin(0.5);
+    const startGameButton = this.add.text(positions.centerX, positions.centerY, 'Start Game!', { 
+        fill: '#FFFFFF', 
+        fontSize: 50,
+        color: "#FFFFFF",
+    }).setOrigin(0.5);
+
+    startGameButton.setInteractive();
+
+    startGameButton.on('pointerover', () => { console.log('add highlight animation'); });
+    startGameButton.on('pointerout', () => { console.log('remove highlight animation'); });
+    startGameButton.on('pointerdown', () => { console.log('add pressing animation'); });
+    startGameButton.on('pointerup', () => { console.log('start game scene'); });
+
     }
 
 
